922e8aeb76717b2377f67bcc1d0ebeea98b2665e
LCMedeiros <dlucastjf@gmail.com>
2025-06-22 17:25:54 -0300

    nova atualização de conexão no bd

--- include/connect.php
+++ include/connect.php
@@ -7,7 +7,7 @@
 ?> -->
 
 <?php
-$host = "meu-banco-php.123456789012.us-east-1.rds.amazonaws.com"; // Endpoint que você copiou
+$host = "sqlnovo.chhb1lexyp6c.us-east-1.rds.amazonaws.com"; // Endpoint que você copiou
 $user = "admin1"; // Ou o nome de usuário que você definiu ao criar o banco
 $password = "Admin123,"; // A senha mestra que você criou
 $dbname = "sqlnovo"; // Nome do banco de dados
